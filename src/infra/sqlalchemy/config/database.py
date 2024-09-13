9152bdb81fe9dd9b00e216be2b79b48cd9b2e657
thalesapenas <thales.rodrigues@ufpi.edu.br>
2024-09-13 13:16:37 -0300

    update: postgress db

--- src/infra/sqlalchemy/config/database.py
+++ src/infra/sqlalchemy/config/database.py
@@ -2,11 +2,11 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "sqlite:///./dashboard_database.db"
+SQLALCHEMY_DATABASE_URL = "postgresql://dashboardbd_user:3igR7So9Z0m2jqWrUIKKVb1hXqJ0go7W@dpg-cri64d3v2p9s73bl3nc0-a.oregon-postgres.render.com/dashboardbd"
 #SQLALCHEMY_DATABASE_URL = "postgresql://user:password@postgresserver/db"
 
 engine = create_engine(
-    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
+    SQLALCHEMY_DATABASE_URL
 )
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
